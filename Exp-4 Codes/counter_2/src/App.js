04224312d6b4474e0b64d7dec1e9ca1ee9e7926c
Soham Walam <147302133+SohamWalam11@users.noreply.github.com>
2025-04-25 19:16:13 +0530

    Add files via upload

---
+++ Exp-4 Codes/counter_2/src/App.js
@@ -0,0 +1,15 @@
+// App.js
+import React from 'react';
+import Counter from './components/Counter';
+import './components/style_1.css';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Welcome to the Counter App</h1>
+            <Counter />
+        </div>
+    );
+};
+
+export default App;
